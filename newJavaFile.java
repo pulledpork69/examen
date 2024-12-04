fca59fe991b7213a308376e4be56243826c6292b
FLORES BURILLO <alexandrefloresburillo@gmail.com>
2024-12-04 20:26:03 +0100

    Base

---
+++ newJavaFile.java
@@ -0,0 +1,22 @@
+
+package javaapplication1;
+import java.util.Scanner;
+public class newJavaFile {
+public static void main(String args[]) {
+    Scanner sc= new Scanner(System.in);
+System.out.println("Menú. 1:Sumar 2:Restar 3:Multiplicar 4:Dividir");
+int menu=sc.nextInt();
+int num1,num2;
+switch(menu){
+    case 1: 
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+}
+    }
+}
+
