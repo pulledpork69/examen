eebce22d1511a165802e62456a212b6a86f94526
FLORES BURILLO <alexandrefloresburillo@gmail.com>
2024-12-04 20:29:56 +0100

    Entrada Suma

--- newJavaFile.java
+++ newJavaFile.java
@@ -8,7 +8,10 @@ System.out.println("Menú. 1:Sumar 2:Restar 3:Multiplicar 4:Dividir");
 int menu=sc.nextInt();
 int num1,num2;
 switch(menu){
-    case 1: 
+    case 1: System.out.println("Dame el primer número");
+                num1=sc.nextInt();
+            System.out.println("Dame el segundo número");
+                num2=sc.nextInt();
         break;
     case 2:
         break;
