957e744ee28cbf533e541e247366947e81a991ad
FLORES BURILLO <alexandrefloresburillo@gmail.com>
2024-12-05 16:52:09 +0100

    Salida rama3

--- ejercicio.java
+++ ejercicio.java
@@ -20,10 +20,24 @@ public class ejercicio {
         String frase = sc.nextLine();
         System.out.print("Ingrese una letra para buscar: ");
         String letraInput = sc.nextLine();
-     
+       if (letraInput.length() != 1) {
+            System.out.println("Por favor, ingrese solo una letra.");
+        } else {
+            char letra = letraInput.charAt(0);
+            int contador = 0;
+
+            for (int i = 0; i < frase.length(); i++) {
+                if (frase.charAt(i) == letra) {
+                    contador++;
+                }
+            }
+            System.out.println("La letra '" + letra + "' aparece " + contador + " veces en la frase.");
+       }
+         
      case 4:
  }
     }
 }
+
     
 
