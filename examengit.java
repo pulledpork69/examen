fe8f02f9862a493e9680a99d675386f74e7f6584
FLORES BURILLO <alexandrefloresburillo@gmail.com>
2024-12-05 16:24:56 +0100

    Base

---
+++ examengit.java
@@ -0,0 +1,25 @@
+
+package javaapplication1;
+import java.util.Scanner;
+
+public class examengit {
+    public static void main(String args[]) {
+         Scanner sc=new Scanner(System.in);
+        String palabra1,palabra2;
+ System.out.println("Menú. 1:Concatenar 2:Número palabras 3:Número letra 4:Salir");
+ int eleccion;
+ eleccion=sc.nextInt();
+ switch (eleccion){
+     case 1: System.out.println("Dame una palabra");
+               palabra1=sc.nextLine();
+             System.out.println("Dame otra palabra");
+               palabra2=sc.nextLine();
+     
+     case 2:
+         
+     case 3:
+     
+     case 4:
+ }
+    }
+}
